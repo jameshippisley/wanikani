9c9808b14e0fc615758f82b1b2ed85134a594ca3
jameshippisley <robinson.jh@gmail.com>
2018-08-31 09:29:26 -0400

    Create rendaku_informaton.js

---
+++ user_scripts/rendaku_informaton.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name        WaniKani Rendaku Information
+// @version     0.1
+// @author      jameshippisley
+// @description Adds information to Wanikani about why readings do or do not use rendaku.
+// @license     GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @namespace   wk_rendaku
+//
+// @match      *://www.wanikani.com/*vocabulary/*
+// @match      *://www.wanikani.com/review/session*
+// @match      *://www.wanikani.com/lesson/session*
+//
+// @updateURL   https://github.com/jameshippisley/wanikani/raw/master/user_scripts/rendaku_information.js
+// @downloadURL https://https://github.com/jameshippisley/wanikani/raw/master/user_scripts/rendaku_information.js
+//
+// //@resource    kanji_db     https://raw.githubusercontent.com/mwil/wanikani-userscripts/master/wanikani-phonetic-compounds/db/kanji_esc.json
+//
+// //@require     https://raw.githubusercontent.com/jameshippisley/wanikani-userscripts/3c6ea0466951a1e58b75b2cb7a945ed7716082f7/utility/wk_interaction.js
+// @require https://github.com/jameshippisley/wanikani/raw/master/user_scripts/rendaku_information_include.js
+//
+// @run-at      document-end
+// ==/UserScript==
