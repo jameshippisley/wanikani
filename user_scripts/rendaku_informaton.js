0d4b84232afea7154613db9caa6108c4fb425253
jameshippisley <robinson.jh@gmail.com>
2018-08-31 10:48:17 -0400

    Update rendaku_informaton.js

--- user_scripts/rendaku_informaton.js
+++ user_scripts/rendaku_informaton.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        WaniKani Rendaku Information
-// @version     0.1001
+// @version     0.1002
 // @author      jameshippisley
 // @description Adds information to Wanikani about why readings do or do not use rendaku.
 // @license     GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
@@ -30,25 +30,27 @@ $(function() {
         }
     }
 
-/*    // Process the review page.
+    // Process the review page.
     else if (url.indexOf('review/session') != -1) {
+        var current_item = ''
+
         // Display the information when the current item changes. Don't do this if they are
         // supposed to enter the reading.
         $.jStorage.listenKeyChange('currentItem', function(key) {
-            var section = GetPartOfSpeechData($.jStorage.get(key).voc, DATA);
-            if (section && $('#answer-form input').attr('lang') != 'ja') {
-                $('div#item-info').prepend(section.append('<br />'));
+            var section = '<h2>Rendaku Information</h2><p>Your notes</p>'
+            if (section && $('#answer-form input').attr('lang') == 'ja' && current_item != 'ok') {
+                $('section#note-reading').before(section);
             }
         });
 
-        // If the 'all-info' button is pressed, then display it.
         $('div#all-info').on('click', function() {
-            var section = GetPartOfSpeechData($.jStorage.get('currentItem').voc, DATA);
-            if (section) {
-                $('div#item-info').prepend(section.append('<br />'));
+            var section = '<h2>Rendaku Information</h2><p>Your notes</p>'
+            if (section && current_item != 'ok') {
+                current_item = 'ok'
+                $('section#note-reading').before(section);
             }
         });
-    } */
+    }
 
     // Process the lesson page.
     else if (url.indexOf('lesson/session') != -1) {
