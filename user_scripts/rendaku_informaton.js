df5de8d908319a5d727d275e5a491a4035d5baa7
jameshippisley <robinson.jh@gmail.com>
2018-08-31 11:15:48 -0400

    Update rendaku_informaton.js

--- user_scripts/rendaku_informaton.js
+++ user_scripts/rendaku_informaton.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        WaniKani Rendaku Information
-// @version     0.1002
+// @version     0.1003
 // @author      jameshippisley
 // @description Adds information to Wanikani about why readings do or do not use rendaku.
 // @license     GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
@@ -19,12 +19,17 @@
 $(function() {
     'use strict';
 
+    function get_rendaku_section(word) {
+        return `<h2>Rendaku Information</h2><p>Your notes for ${word}</p>`;
+    }
+
     // Switch based on the content of the URL.
     var url = document.URL;
 
     // Process the vocabulary page.
     if (url.indexOf('vocabulary') != -1) {
-        var section = '<h2>Rendaku Information</h2><p>Your notes</p>'
+        var word = $('header span.vocabulary-icon span').text().trim();
+        var section = get_rendaku_section(word);
         if (section) {
             $('section#note-reading').before(section);
         }
@@ -32,22 +37,25 @@ $(function() {
 
     // Process the review page.
     else if (url.indexOf('review/session') != -1) {
-        var current_item = ''
+        var current_word = ''
 
         // Display the information when the current item changes. Don't do this if they are
         // supposed to enter the reading.
         $.jStorage.listenKeyChange('currentItem', function(key) {
-            var section = '<h2>Rendaku Information</h2><p>Your notes</p>'
-            if (section && $('#answer-form input').attr('lang') == 'ja' && current_item != 'ok') {
+            var word = $.jStorage.get(key).voc;
+            var section = get_rendaku_section(word);
+            if (section && $('#answer-form input').attr('lang') == 'ja' && current_word != word) {
                 $('section#note-reading').before(section);
+                current_word = word;
             }
         });
 
         $('div#all-info').on('click', function() {
-            var section = '<h2>Rendaku Information</h2><p>Your notes</p>'
-            if (section && current_item != 'ok') {
-                current_item = 'ok'
+            var word = $.jStorage.get(key).voc;
+            var section = get_rendaku_section(word);
+            if (section && current_word != word) {
                 $('section#note-reading').before(section);
+                current_word = word;
             }
         });
     }
@@ -55,7 +63,8 @@ $(function() {
     // Process the lesson page.
     else if (url.indexOf('lesson/session') != -1) {
         $.jStorage.listenKeyChange('l/currentLesson', function(key) {
-            var section = '<h2>Rendaku Information</h2><p>Your notes</p>'
+            var word = $.jStorage.get(key).voc;
+            var section = get_rendaku_section(word);
             if (section) {
                 $('div#supplement-voc-reading-exp').after(section);
             }
