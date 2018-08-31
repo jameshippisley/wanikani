7acc7af87d013dca6e1d55f3d6f162f544a22624
jameshippisley <robinson.jh@gmail.com>
2018-08-31 09:42:57 -0400

    Update rendaku_informaton.js

--- user_scripts/rendaku_informaton.js
+++ user_scripts/rendaku_informaton.js
@@ -13,10 +13,50 @@
 // @updateURL   https://github.com/jameshippisley/wanikani/raw/master/user_scripts/rendaku_information.js
 // @downloadURL https://https://github.com/jameshippisley/wanikani/raw/master/user_scripts/rendaku_information.js
 //
-// //@resource    kanji_db     https://raw.githubusercontent.com/mwil/wanikani-userscripts/master/wanikani-phonetic-compounds/db/kanji_esc.json
-//
-// //@require     https://raw.githubusercontent.com/jameshippisley/wanikani-userscripts/3c6ea0466951a1e58b75b2cb7a945ed7716082f7/utility/wk_interaction.js
-// @require https://github.com/jameshippisley/wanikani/raw/master/user_scripts/rendaku_information_include.js
-//
 // @run-at      document-end
 // ==/UserScript==
+
+$(function() {
+    'use strict';
+
+    // Switch based on the content of the URL.
+    var url = document.URL;
+
+    // Process the vocabulary page.
+    if (url.indexOf('vocabulary') != -1) {
+        var section = '<h2>Rendaku Information</h2><p>Your notes</p>'
+        if (section) {
+            $('section#note-reading').before(section);
+        }
+    }
+
+/*    // Process the review page.
+    else if (url.indexOf('review/session') != -1) {
+        // Display the information when the current item changes. Don't do this if they are
+        // supposed to enter the reading.
+        $.jStorage.listenKeyChange('currentItem', function(key) {
+            var section = GetPartOfSpeechData($.jStorage.get(key).voc, DATA);
+            if (section && $('#answer-form input').attr('lang') != 'ja') {
+                $('div#item-info').prepend(section.append('<br />'));
+            }
+        });
+
+        // If the 'all-info' button is pressed, then display it.
+        $('div#all-info').on('click', function() {
+            var section = GetPartOfSpeechData($.jStorage.get('currentItem').voc, DATA);
+            if (section) {
+                $('div#item-info').prepend(section.append('<br />'));
+            }
+        });
+    }
+
+    // Process the lesson page.
+    else if (url.indexOf('lesson/session') != -1) {
+        $.jStorage.listenKeyChange('l/currentLesson', function(key) {
+            var section = GetPartOfSpeechData($.jStorage.get(key).voc, DATA);
+            if (section) {
+                $('div#supplement-voc-meaning').append(section.prepend('<br />'));
+            }
+        });
+    } */
+});
